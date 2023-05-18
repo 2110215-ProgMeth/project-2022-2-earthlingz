d530947816ecf0c693cae6e0db0df8a1c044dc1a
Pathorn Roungtan <junior254789@gmail.com>
2023-05-19 01:18:58 +0700

    implement physics logic and rendering

--- src/config/Config.java
+++ src/config/Config.java
@@ -4,6 +4,8 @@ public class Config {
 	
 	public static final double screenWidth = 1280;
 	public static final double screenHeight = 800;
+	public static final double earthlingWidth = 64;
+	public static final double earthlingHeight = 64;
 	public static double gravity = 10;
 
 }
